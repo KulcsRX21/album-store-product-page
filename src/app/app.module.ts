35ece23ecee0795a2b19d3de84280c61eb94c5d1
KulcsRX21 <kulcsrx@gmail.com>
2022-11-01 18:45:06 +0100

    module 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ProductDescriptionComponent } from './product-description/product-descr
 import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { RouterModule, ROUTES, Routes } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -21,9 +22,15 @@ import { ProductListComponent } from './product-list/product-list.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'products', component: ProductListComponent},
+      {path: 'product/:id', component: ProductPageComponent},
+      {path: '', redirectTo: 'products', pathMatch: 'full'}
+    ])
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
