13d7103dc36244e2644081acf052ecb8bf20f4bd
KulcsRX21 <kulcsrx@gmail.com>
2022-11-01 18:25:45 +0100

    module 6

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,12 +2,14 @@ import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { AlbumArtist } from './albumArtist';
+import { Product } from './product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
   private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/products.json'; 
 
   constructor(private _http: HttpClient) { }
 
@@ -15,5 +17,11 @@ export class ProductService {
     return this._http.get<AlbumArtist>(this._albumUrl).pipe(
       map((albumArtist) => albumArtist
     ));
+  }
+
+  getProducts(): Observable<Product[]> {
+    return this._http.get<Product[]>(this._productsUrl).pipe(
+      map((product) => product)
+    );
   }
 }
